6340c5c3940d441094057695d3e070312dd4d51a
Yazak <yoosufyazak@gmail.com>
2024-01-24 17:18:23 +0500

    added sign in page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,50 +5,71 @@ import { useRouter } from "next/navigation";
 import Section from "./_components/section";
 import Form from "./_components/form";
 import Navbar from "./_components/navbar/navbar";
+import { useState } from "react";
+import SignIn from "./_components/signin";
+import Cookies from "js-cookie";
 
 export default function Home() {
     const router = useRouter();
+    const [auth, setAuth] = useState(Cookies.get("userAuth"));
+
+    const login = () => {
+        Cookies.set("userAuth", true);
+    };
 
     const handleLoginClick = () => {
         router.push("/dashboard");
     };
+
+    const handleLogoutClick = () => {
+        Cookies.remove("userAuth");
+        window.location.reload();
+    };
+
     return (
         <div>
             <Navbar
-                routeLogin={handleLoginClick}
-                routeHome={() => "pass"}
-                Route=""
-                Submit="Submit"
-                Login="Login"
+                routeLogin={handleLogoutClick}
+                routeHome={handleLoginClick}
+                Route="Dashboard"
+                Login="Logout"
+                Submit=""
             />
-            <div className="min-h-screen max-w-screen-xl mx-auto">
-                <div className="px-24">
-                    <Section />
-                    <div className="font-poppins">
-                        <div>
-                            <div>
-                                <div className="font-semibold text-gray-600 pb-1">
-                                    SUBMIT
+            <div>
+                {auth ? (
+                    <div className="min-h-screen max-w-screen-xl mx-auto">
+                        <div className="px-24">
+                            <Section />
+                            <div className="font-poppins">
+                                <div>
+                                    <div>
+                                        <div className="font-semibold text-gray-600 pb-1">
+                                            SUBMIT
+                                        </div>
+                                        <div className="text-3xl mb-16 font-semibold">
+                                            Show us
+                                        </div>
+                                    </div>
+                                    <div>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit. Vestibulum ornare
+                                        sapien enim, vel finibus risus posuere
+                                        eu. Maecenas placerat diam a sem congue,
+                                        et vestibulum metus varius. Praesent in
+                                        dui ligula. Donec pulvinar neque vitae
+                                        vulputate semper.
+                                    </div>
                                 </div>
-                                <div className="text-3xl mb-16 font-semibold">
-                                    Show us
+                                <div className="max-w-screen-sm">
+                                    <Form />
                                 </div>
                             </div>
-                            <div>
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Vestibulum ornare sapien enim,
-                                vel finibus risus posuere eu. Maecenas placerat
-                                diam a sem congue, et vestibulum metus varius.
-                                Praesent in dui ligula. Donec pulvinar neque
-                                vitae vulputate semper.
-                            </div>
-                        </div>
-                        <div className="max-w-screen-sm">
-                            <Form />
+                            <div className="pt-20 pb-52"></div>
                         </div>
                     </div>
-                    <div className="pt-20 pb-52"></div>
-                </div>
+                ) : (
+                    <SignIn signin={login} />
+                )}
             </div>
         </div>
     );
